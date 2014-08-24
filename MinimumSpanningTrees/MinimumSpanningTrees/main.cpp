5bb7200a3b28bac16cc80021605fe2d10fcdb419
RogerChen <chenyuntao08@gmail.com>
2014-08-24 18:13:28 +0800

    init commit of MST

---
+++ MinimumSpanningTrees/MinimumSpanningTrees/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  MinimumSpanningTrees
+//
+//  Created by RogerChen on 14-8-24.
+//  Copyright (c) 2014年 RogerChen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
